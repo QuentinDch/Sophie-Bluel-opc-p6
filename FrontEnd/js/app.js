17592e20138cedab1590bb4bad6e712154e2e33f
Quentin <quentin.caflo@gmail.com>
2024-08-21 13:13:03 +0200

    Refactor  to use  and extract  function

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,3 +1,6 @@
+// Récupération des travaux depuis le back-end
+// Appel à l’API avec fetch afin de récupérer dynamiquement les projets
+// Ajout des travaux à la galerie
 async function fetchGalleryData() {
   const worksApiUrl = "http://localhost:5678/api/works";
   try {
@@ -11,29 +14,35 @@ async function fetchGalleryData() {
 
     const galleryDivElement = document.querySelector(".gallery");
 
-    for (let i = 0; i < galleryData.length; i++) {
-      const project = galleryData[i];
-
-      let worksFigElement = document.createElement("figure");
-      worksFigElement.id = `project-${project.id}`;
-
-      let worksImgElement = document.createElement("img");
-      worksImgElement.src = project.imageUrl;
-      worksImgElement.alt = project.title;
-
-      let worksFigcaptionElement = document.createElement("figcaption");
-      worksFigcaptionElement.textContent = project.title;
-
+    galleryData.forEach((project) => {
+      const worksFigElement = createGalleryElement(project);
       galleryDivElement.appendChild(worksFigElement);
-      worksFigElement.append(worksImgElement, worksFigcaptionElement);
-    }
+    });
   } catch (error) {
-    console.error(error.message);
+    console.error("Error fetching gallery data:", error.message);
   }
+}
+
+function createGalleryElement(project) {
+  const worksFigElement = document.createElement("figure");
+  worksFigElement.id = `project-${project.id}`;
+
+  const worksImgElement = document.createElement("img");
+  worksImgElement.src = project.imageUrl;
+  worksImgElement.alt = project.title;
+  worksImgElement.loading = "lazy";
+
+  const worksFigcaptionElement = document.createElement("figcaption");
+  worksFigcaptionElement.textContent = project.title;
+
+  worksFigElement.append(worksImgElement, worksFigcaptionElement);
+
+  return worksFigElement;
 }
 
 fetchGalleryData();
 
+// Réalisation du filtre des travaux : Ajout des filtres pour afficher les travaux par catégorie
 async function fetchCategoriesGalleryData() {
   const categoriesGalleryUrl = "http://localhost:5678/api/categories";
   try {
