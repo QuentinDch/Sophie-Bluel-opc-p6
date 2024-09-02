19e870507e0e0ab7fb58b2e7b9a9a684a49cac5a
Quentin <quentin.caflo@gmail.com>
2024-09-02 11:31:49 +0200

    Function to manage form submission

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -20,5 +20,24 @@ async function loginUser(email, password) {
     console.error("Error during user login:", error.message);
   }
 }
-// Appel de la fonction avec des paramètres
+// Appel de la fonction avec les paramètres
 loginUser("sophie.bluel@test.tld", "S0phie");
+
+// Fonction pour gérer la soumission du formulaire et appeler loginUser
+function setupFormHandler() {
+  const formElement = document.getElementById("contact");
+  formElement.addEventListener("submit", async (e) => {
+    e.preventDefault(); // Empêche le rechargement de la page ou l'envoi du formulaire
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const userEmail = emailInput.value;
+    const userPassword = passwordInput.value;
+
+    // Appeler loginUser avec les valeurs du formulaire
+    const userData = await loginUser(userEmail, userPassword);
+
+    // Traiter les données utilisateur ou gérer les erreurs ici
+    console.log(userData);
+  });
+}
+setupFormHandler();
